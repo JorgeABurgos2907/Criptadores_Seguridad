bd285e8c8170bb524fc0a7841abc580f20ff6fb1
Jorge Burgos <burgosjorge2907@gmail.com>
2021-12-07 17:42:04 -0500

    texto playFair listo

--- PlayFair.js
+++ PlayFair.js
@@ -36,3 +36,38 @@ for(i=0;i<Abecedario_clave.length;i++){
 console.table(matrix)
 
 mensaje = "WITH A LITTLE HELP FROM MY FRIENDS";
+mensaje = mensaje.replaceAll(" ","")
+mensaje = mensaje.split('');
+
+cadaDos = 0;
+mensaDos = [];
+resul = '';
+
+for(i=0;i<mensaje.length;i++){
+
+    resul = resul + mensaje[i]
+    cadaDos = cadaDos + 1;
+
+    if(mensaje[i]==mensaje[i+1]){
+
+        if(cadaDos == 1){
+            resul = resul + 'X'
+            cadaDos = cadaDos + 1;
+        }
+        
+    }
+
+    if(cadaDos == 2){
+        mensaDos.push(resul);
+        cadaDos = 0;
+        resul = '';
+    }
+
+    if(i == (mensaje.length-1) && cadaDos == 1 ){
+        resul = resul + 'X'
+        mensaDos.push(resul);
+    }
+
+}
+
+console.log(mensaDos)
