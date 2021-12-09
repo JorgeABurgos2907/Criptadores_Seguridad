74e0aa1ce5d43d116750f06e6e144bca943b3f50
Jorge Burgos <burgosjorge2907@gmail.com>
2021-12-09 12:11:30 -0500

    multiplicacion matrices

---
+++ Hill.js
@@ -0,0 +1,26 @@
+const math = require('mathjs');
+const adjunto = require('./inverse');
+
+function multiply(a, b) {
+    var aNumRows = a.length, aNumCols = a[0].length,
+        bNumRows = b.length, bNumCols = b[0].length,
+        m = new Array(aNumRows);  // initialize array of rows
+    for (var r = 0; r < aNumRows; ++r) {
+      m[r] = new Array(bNumCols); // initialize the current row
+      for (var c = 0; c < bNumCols; ++c) {
+        m[r][c] = 0;             // initialize the current cell
+        for (var i = 0; i < aNumCols; ++i) {
+          m[r][c] += a[r][i] * b[i][c];
+        }
+      }
+    }
+    return m;
+  }
+
+const transpose = (arr) => {
+    return Object.keys(arr[0]).map(function (c) {
+        return arr.map(function (r) {
+            return r[c];
+        });
+    });
+}
